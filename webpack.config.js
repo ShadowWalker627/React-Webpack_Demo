668aeb0a741d7e3edfe4d04b5eca0c91211c9221
ShadowWalker627 <381169437@qq.com>
2016-10-11 11:31:56 +0800

    加入浏览器自动刷新

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './app/main.js')],
+    entry: [
+        'webpack/hot/dev-server', 
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, './app/main.js')
+    ],
     output: {
         path: path.resolve(__dirname, './build'),
         filename: 'bundle.js'
