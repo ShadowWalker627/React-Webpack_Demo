b1f909e35e6a671e9d79cd2e3a0e892c6ba2c2dd
ShadowWalker627 <381169437@qq.com>
2016-10-11 09:19:13 +0800

    react-webpack-template

---
+++ app/component.js
@@ -0,0 +1,11 @@
+/**
+ * Created by baymax on 16/10/9.
+ */
+'use strict';
+import React from 'react';
+
+export default class Hello extends React.Component {
+    render() {
+        return <h1>Hello world</h1>;
+    }
+}
